9c489b670d33ffdae2c9147f865844e5374447a9
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-03-02 21:33:27 +0100

    Create AccountingServiceApplication.java

---
+++ accounting-service/src/main/java/com/accountingengine/accounting/AccountingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.accountingengine.accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingServiceApplication.class, args);
+    }
+}
